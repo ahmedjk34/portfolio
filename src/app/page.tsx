7f051cb2ba1545b0248c6261e45130e06ac8ef03
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-07 18:02:10 +0300

    Some work on the hero of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,18 @@ import Image from "next/image";
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className={styles.hero}>
+      <div className={styles.heroInfo}>
+        <p>Hello, my name is</p>
+        <h1>Ahmed Gharib</h1>
+        <p>
+          Full stack web developer specializing in NextJS with Typescript, SCSS
+          for styling, MongoDB for databases, I'm currently at my third-year
+          student in computer engineering at NNU with a 3.96 GPA.
+        </p>
+        <span></span>
+      </div>
+    </div>
+  );
 }
