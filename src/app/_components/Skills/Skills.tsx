56ef9fe1d5da95dde22c607d75262863ad90eb3d
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-08 21:04:18 +0300

    Added the skills section

---
+++ src/app/_components/Skills/Skills.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./skills.module.scss";
+type Props = {};
+
+function Skills({}: Props) {
+  return (
+    <div className={styles.skillsSection} id="skills">
+      Skills
+    </div>
+  );
+}
+
+export default Skills;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import AboutMe from "./_components/AboutMe/AboutMe";
 import Hero from "./_components/Hero/Hero";
+import Skills from "./_components/Skills/Skills";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <AboutMe />
+      <Skills />
     </>
   );
 }
