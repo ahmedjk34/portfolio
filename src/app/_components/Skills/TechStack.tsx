f015329c6c169d0050ca60fb1612d551975e6bf4
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-08 22:20:23 +0300

    Some work on the tech stack slider (not final)

--- src/app/_components/Skills/TechStack.tsx
+++ src/app/_components/Skills/TechStack.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./techStack.module.scss";
 import techStack from "./techStackData";
+import ScrollArrow from "../ScrollArrow/ScrollArrow";
 type Props = {};
 
 function Skills({}: Props) {
@@ -8,17 +9,32 @@ function Skills({}: Props) {
     <div className={styles.skillsSection} id="tech-stack">
       <h1>Tech Stack</h1>
       <div className={styles.techSlider}>
-        {techStack.map((tech) => (
-          <div className={styles.tech}>
-            <img
-              src={tech.image}
-              alt={tech.title}
-              className={styles.techIcon}
-            />
-            <h3>{tech.title}</h3>
-          </div>
-        ))}
+        <div className={styles.techSlide}>
+          {techStack.map((tech) => (
+            <div className={styles.tech}>
+              <img
+                src={tech.image}
+                alt={tech.title}
+                className={styles.techIcon}
+              />
+              <h3>{tech.title}</h3>
+            </div>
+          ))}
+        </div>
+        <div className={styles.techSlide}>
+          {techStack.map((tech) => (
+            <div className={styles.tech}>
+              <img
+                src={tech.image}
+                alt={tech.title}
+                className={styles.techIcon}
+              />
+              <h3>{tech.title}</h3>
+            </div>
+          ))}
+        </div>
       </div>
+      <ScrollArrow id="projects" />
     </div>
   );
 }
