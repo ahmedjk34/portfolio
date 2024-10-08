658608e2b5764e319fcacb77f824bda6ac4ae308
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-08 21:51:29 +0300

    Created the tech stacks array

---
+++ src/app/_components/Skills/techStackData.ts
@@ -0,0 +1,18 @@
+const techStack = [
+  { title: "Git", image: "/images/tech/git-color.svg" },
+  { title: "Figma", image: "/images/tech/figma-color.svg" },
+  { title: "HTML5", image: "/images/tech/html5-color.svg" },
+  { title: "CSS3", image: "/images/tech/css3-color.svg" },
+  { title: "JavaScript", image: "/images/tech/javascript-color.svg" },
+  { title: "TypeScript", image: "/images/tech/typescript-color.svg" },
+  { title: "MongoDB", image: "/images/tech/mongodb-color.svg" },
+  { title: "Mongoose", image: "/images/tech/mongoose-color.svg" },
+  { title: "ExpressJS", image: "/images/tech/expressjs-color.svg" },
+  { title: "NodeJS", image: "/images/tech/nodejs-color.svg" },
+  { title: "NextJS", image: "/images/tech/nextjs-color.svg" },
+  { title: "React", image: "/images/tech/react-color.svg" },
+  { title: "SASS", image: "/images/tech/sass-color.svg" },
+  { title: "Next-Auth", image: "/images/tech/next-auth-color.svg" },
+  { title: "Firebase", image: "/images/tech/firebase-color.svg" },
+];
+export default techStack;
