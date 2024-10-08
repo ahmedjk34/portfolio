cbb622e1f477467c8cf2920727ef1b24075c76f8
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-08 17:37:31 +0300

    Added an arrow to the bottom of the hero, to scroll towards the about me section

--- src/app/_components/Hero/Hero.tsx
+++ src/app/_components/Hero/Hero.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./hero.module.scss";
+import Link from "next/link";
 type Props = {};
 
 function Hero({}: Props) {
   return (
-    <div className={styles.hero}>
+    <div className={styles.hero} id="hero">
       <div className={styles.heroInfo}>
         <p>Hello, my name is</p>
         <h1>Ahmed Gharib</h1>
@@ -13,6 +14,7 @@ function Hero({}: Props) {
           SCSS, and MongoDB, among other technologies. Currently, I am in my
           third year of studying computer engineering at NNU, with a 3.96 GPA.
         </p>
+        <Link className={styles.arrow} href={"#about"}></Link>
       </div>
     </div>
   );
