8e8ce4e448a35029e3069315856a48d476b1cf5b
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-08 16:59:05 +0300

    Extracted the hero section into it's own component

---
+++ src/app/_components/Hero/Hero.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./hero.module.scss";
+type Props = {};
+
+function Hero({}: Props) {
+  return (
+    <div className={styles.hero}>
+      <div className={styles.heroInfo}>
+        <p>Hello, my name is</p>
+        <h1>Ahmed Gharib</h1>
+        <p>
+          I am a full stack web developer specializing in Next.js, TypeScript,
+          SCSS, and MongoDB, among other technologies. Currently, I am in my
+          third year of studying computer engineering at NNU, with a 3.96 GPA.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.scss";
+import Hero from "./_components/Hero/Hero";
 
 export default function Home() {
   return (
-    <div className={styles.hero}>
-      <div className={styles.heroInfo}>
-        <p>Hello, my name is</p>
-        <h1>Ahmed Gharib</h1>
-        <p>
-          Full stack web developer specializing in NextJS with Typescript, SCSS
-          for styling, MongoDB for databases, I'm currently at my third-year
-          student in computer engineering at NNU with a 3.96 GPA.
-        </p>
-        <span></span>
-      </div>
-    </div>
+    <>
+      <Hero />
+    </>
   );
 }
