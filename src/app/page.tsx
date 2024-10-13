a9edca56e20d71d464530977ee871346716b6ad3
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-13 19:10:03 +0300

    Added the styles used for the scroll library in main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import ContactMe from "./_components/ContactMe/ContactMe";
 import Hero from "./_components/Hero/Hero";
 import ProjectsSection from "./_components/ProjectsSection/ProjectsSection";
 import Skills from "./_components/Skills/TechStack";
+import "animate.css/animate.compat.css";
 
 export default function Home() {
   return (
