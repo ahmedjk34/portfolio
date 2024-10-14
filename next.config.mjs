eccd221eedd2886109326a6bd5136e7a8219e2b1
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-14 10:03:23 +0300

    Testing for static images on github pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,6 @@
 
 const nextConfig = {
   basePath: "/portfolio",
-  assetPrefix: "/portfolio", // Ensures assets (images, CSS) are served from correct path
-
-  images: {
-    loader: "default",
-    path: "/portfolio", // Base path for images (public folder in GitHub Pages)
-  },
   output: "export",
   reactStrictMode: true,
 };
