74b4495f803bce00ce3f2be200650d2d2ec48594
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-14 09:51:59 +0300

    Tweaks

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 
 const nextConfig = {
   basePath: "/portfolio",
+  assetPrefix: "/portfolio", // Ensures assets (images, CSS) are served from correct path
+
   images: {
     loader: "default",
     path: "/portfolio", // Base path for images (public folder in GitHub Pages)
